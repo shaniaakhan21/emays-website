4aba18d4130ede3db5a7bb27b01c3f6ebd44d92c
Chathuranga <Hashan3SR@gmail.com>
2023-03-02 09:39:39 +0400

    Added new endpoint for email launch and added test event to dev test html

--- app/route/launchRoute.ts
+++ app/route/launchRoute.ts
@@ -1,13 +1,66 @@
 'use strict';
 
 import * as express from 'express';
+
 const router = express.Router();
 import Logger from '../logger';
 import { RoutePath } from '../const/routePath';
 import { DevLaunchTemplateData, LaunchRequestBody, LaunchUIContext } from '../type/ILaunchContext';
 import { authorizeLaunchRoute, buildAppLaunchPath } from '../api/launchAPI';
 import { config } from '../config/config';
+import * as core from 'express-serve-static-core';
 
+/**
+ * To accept the launch request from email and render the UI
+ */
+router.get(RoutePath.LAUNCH_MAIL, (
+    req: express.Request<core.ParamsDictionary, any, any, { uuid: string, launchType: string }>,
+    res: express.Response, next: express.NextFunction): void => {
+    (async () => {
+        const uuid = req.query.uuid;
+        const launchType = req.query.launchType;
+        Logger.log('info', `Requesting UI for a user with uuid: ${uuid} and launchType: ${launchType}.`);
+        // TODO: Validate if the user uuid is valid
+
+        // TODO: Get the product details from the database
+        const launchTemplateData = [{
+            productName: 'Nike Air Max 270',
+            productColor: 'Black',
+            productSize: 'UK 8',
+            productQuantity: '1',
+            productCost: '£120',
+            productImage: 'https://static.nike.com/a/images/t_PDP_864_v1/' +
+                'f_auto,b_rgb:f5f5f5/8b1b3b1a-1b1a-4b1a-9b1a-1b1a4b1a9b1a/air-max-270-react-shoe-1JZxJx.jpg',
+            productDeliveryInformation: 'Free delivery'
+        }, {
+            productName: 'Nike Air Max 270',
+            productColor: 'White',
+            productSize: 'UK 8',
+            productQuantity: '1',
+            productCost: '£120',
+            productImage: 'https://static.nike.com/a/images/t_PDP_864_v1/' +
+                'f_auto,b_rgb:f5f5f5/8b1b3b1a-1b1a-4b1a-9b1a-1b1a4b1a9b1a/air-max-270-react-shoe-1JZxJx.jpg',
+            productDeliveryInformation: 'Free delivery'
+        }];
+
+        const applicationPath: string = await buildAppLaunchPath(config.UI_APP_ENTRY_POINT);
+        const stringify = JSON.stringify(launchTemplateData);
+        const cleaned = stringify.replace(/\\/g, '');
+
+        const productData = { 'productList': cleaned };
+        return res.render(applicationPath, productData);
+
+    })().catch((error) => {
+        const errorObject: Error = error as Error;
+        Logger.error(`Failed to launch the UI for the order item.
+        Error stack: ${errorObject.stack as string}.`);
+        next(error);
+        /*
+         * Need to display error template in the app.ts since
+         * there is no UI to cater the error message at this stage (due to form submit)
+         */
+    });
+});
 /**
  * To access the dev world, this route will provide you a HTML form
  */
