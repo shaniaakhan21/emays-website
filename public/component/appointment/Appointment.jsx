883c76d8c1bf05e16e31e1d1d320372dcc054d8c
Chathuranga <Hashan3SR@gmail.com>
2023-03-02 13:35:27 +0400

    Added Appointment.jsx

---
+++ public/component/appointment/Appointment.jsx
@@ -0,0 +1,10 @@
+import { Grid } from '@carbon/react';
+const Appointment = () => {
+    return (
+        <Grid className='appointment-page'>
+            APPOINTMENT PAGE
+        </Grid>
+    );
+};
+
+export default Appointment;
