ae1dacd420180fb796b6496a98e8b3d4f15a4a86
Chathuranga <Hashan3SR@gmail.com>
2023-03-08 16:30:27 +0400

    Integration page completed with responsive design and marquee logo animation

--- public/component/retailer/Integration.jsx
+++ public/component/retailer/Integration.jsx
@@ -9,7 +9,12 @@ import ButtonCustom from '../common/ButtonCustom';
 import './../../scss/component/retailer/integration.page.scss';
 
 // Images
-import EmailIcon from '../../icons/email.svg';
+import IntegrationImageOne from '../../images/intergration-1.png';
+import IntegrationImageTwo from '../../images/intergration-2.png';
+import ShopifyLogo from '../../images/logo-shopify.svg';
+import SalesForceLogo from '../../images/logo-salesforce.svg';
+import BigcommerceLogo from '../../images/logo-bigcommerce.svg';
+import CommercetoolsLogo from '../../images/logo-commercetools.svg';
 
 const RetailerLanding = () => {
     const [translate] = useTranslation();
@@ -26,6 +31,31 @@ const RetailerLanding = () => {
             </Column>
             <Column lg={16} md={8} sm={4} xs={4} className='see-more'>
                 <ButtonCustom action={() => {}} text={t('button')} />
+            </Column>
+            {[IntegrationImageOne, IntegrationImageTwo].map(
+                (image, index) => (<Column lg={16} md={8} sm={4} xs={4} className='info-box'>
+                    <div className='image'>
+                        <div className='image-container'>
+                            <img src={image} alt='Integration Sample Image' />
+                        </div>
+                    </div>
+                    <div className='details'>
+                        <h2>{t(`info-box.${index}.title`)}</h2>
+                        <p>{t(`info-box.${index}.description`)}</p>
+                    </div>
+                </Column>))}
+            <Column lg={16} md={8} sm={4} xs={4} className='req-demo'>
+                <ButtonCustom action={() => {}} text={t('button')} />
+            </Column>
+            <Column lg={16} md={8} sm={4} xs={4} className='logos'>
+                <div className='marquee'>
+                    <div className='marquee__group'>
+                        <img src={ShopifyLogo} alt='Shopify Logo' />
+                        <img src={SalesForceLogo} alt='Salesforce Logo' />
+                        <img src={BigcommerceLogo} alt='Commercetools Logo' />
+                        <img src={CommercetoolsLogo} alt='Bigcommerce Logo' />
+                    </div>
+                </div>
             </Column>
         </RetailerLayout>
     );
