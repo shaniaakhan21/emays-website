7e7f39b202918519c3c5568fce3a48dc186e5070
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2023-03-28 13:58:21 +0400

    scroll-down-customer-page

--- public/component/customer/Home.jsx
+++ public/component/customer/Home.jsx
@@ -1,12 +1,21 @@
 import Nav from '../common/Nav';
 import HomeSection from './HomeSection';
+import ShopWithUs from './ShopWithUs';
+import Environment from './Environment';
+import Services from './Services';
+import RetailerLetsTalk from '../retailer/LetsTalk';
+import RetailerFAQs from '../retailer/FAQs';
 
 const CustomerHome = () => {
     return (
         <>
             <Nav />
             <HomeSection />
-        
+            <ShopWithUs />
+            <Environment />
+            <Services />
+            <RetailerLetsTalk />
+            <RetailerFAQs />
         </>
     );
 };
