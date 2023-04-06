e491b32c84f83565e30783eb2f50f489a400cd2f
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2023-04-06 08:11:11 +0400

    Redesign retailer

--- public/component/retailer/Landing.jsx
+++ public/component/retailer/Landing.jsx
@@ -4,22 +4,14 @@ import { useTranslation } from 'react-i18next';
 // Components
 import RetailerLayout from '../common/RetailerLayout';
 import ButtonCustom from '../common/ButtonCustom';
+import Footer from '../common/Footer';
 
 // SCSS
 import './../../scss/component/retailer/landing.page.scss';
 
 // Images
-import EmailIcon from '../../icons/email.svg';
 import RetailerPartnership from './Partnership';
-import EnvironmentHeader from '../common/environment/EnvironmentHeader';
-import IconTextImg from '../common/environment/IconTextImg';
-import IconImgText from '../common/environment/IconImgText';
-import ImgTextIcon from '../common/environment/ImgTextIcon';
-import TextImg from '../common/environment/TextImg';
-import RetailerIntegration from './Integration';
-import RetailerFAQs from './FAQs';
-import RetailerLetsTalk from './LetsTalk';
-import { useRef } from 'react';
+import MailIcon from '../../images/HomeCustomerSection/email-icon.svg';
 
 const RetailerLanding = () => {
     const [translate] = useTranslation();
@@ -38,31 +30,22 @@ const RetailerLanding = () => {
                 {t('description')}
             </Column>
             <Column lg={16} md={8} sm={4} xs={4} className='see-more'>
-                <ButtonCustom action={() => {}} text={t('button')} />
+                <div data-aos='fade-in' data-aos-easing='linear' 
+                    data-aos-duration='1000' className='shipping-section__right buttons'>
+                    <ButtonCustom action={() => {}} text={t('button')} />
+                    <ButtonCustom
+                        action={() => {}}
+                        iconDescription='E-Mail'
+                        hasIconOnly
+                        renderIcon={() => <img src={MailIcon} alt='E-Mail' />}
+                    />
+                </div>
             </Column>
             <Column lg={16} md={8} sm={4} xs={4} className='buttons'>
             </Column>
-            <Column id='integration-start' lg={16} md={8} sm={4} xs={4}>
-                <RetailerIntegration />
-            </Column>
-            <Column id='faqs-start' lg={16} md={8} sm={4} xs={4}>
-                <RetailerFAQs />
-            </Column>
-
-            <Column id='sustainability-start' lg={16} md={8} sm={4} xs={4} style={{ padding: 0 }}>
-                <EnvironmentHeader />
-                <IconTextImg />
-                <IconImgText />
-                <ImgTextIcon/>
-                <TextImg/>
-            </Column>
 
             <RetailerPartnership />
-
-            <Column id='lets-talk-start' lg={16} md={8} sm={4} xs={4}>
-                <RetailerLetsTalk />
-            </Column>
-
+            <Footer/>
         </RetailerLayout>
     );
 };
--- public/component/retailer/Partnership.jsx
+++ public/component/retailer/Partnership.jsx
@@ -2,22 +2,19 @@ import { Column as Col, Column } from '@carbon/react';
 import { useTranslation } from 'react-i18next';
 import React, { useEffect } from 'react';
 import AOS from 'aos';
-import 'aos/dist/aos.css';
+import 'aos/dist/aos.css';                                                                                  
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
 
-// Components
+//                                                                   Components
 import RetailerLayout from '../common/RetailerLayout';
 
-// SCSS
+//                                                                   SCSS
 import '../../scss/component/retailer/partnership.page.scss';
 import Chart from '../common/Chart';
-import ButtonCustom from '../common/ButtonCustom';
-import EmailIcon from '../../icons/email.svg';
 import LouisVuittonLogo from '../../images/logo-louisVuitton.svg';
 import YSLLogo from '../../images/logo-ysl.svg';
-import ChanelPradaLogo from '../../images/logo-chanel-prada.svg';
 import PRADA from '../../images/HomeCustomerSection/PRADA.svg';
 import CHANEL from '../../images/HomeCustomerSection/CHANEL.svg';
 import CASSANDRE from '../../images/HomeCustomerSection/CASSANDRE.svg';
@@ -47,8 +44,8 @@ const RetailerPartnership = () => {
                 <Col lg={16} md={8} sm={4} xs={4} data-aos='fade-in' data-aos-easing='linear' data-aos-duration='1000'>
                     <h1 className='header'>{t('title')}</h1>
                 </Col>
-                <Col lg={8} md={5} sm={4} xs={4} className='infos'>
-                    {[0, 1, 2, 3, 4, 5].map((idx) => (<div 
+                <Col lg={16} md={5} sm={4} xs={4} className='infos-one'>
+                    {[0, 1].map((idx) => (<div 
                         key={idx}
                         data-aos='fade-right'
                         data-aos-easing='linear'
@@ -61,10 +58,66 @@ const RetailerPartnership = () => {
                         
                     </div>))}
                 </Col>
-                <Col lg={8} md={3} sm={4} xs={4} />
-                <Col lg={16} md={8} sm={4} xs={4}>
-                    <h2 className='chart-title'>{t('chart-title')}</h2>
+                <Col lg={16} md={5} sm={4} xs={4} className='infos-one'>
+                    {[2].map((idx) => (<div 
+                        key={idx}
+                        data-aos='fade-right'
+                        data-aos-easing='linear'
+                        data-aos-duration='1000'
+                        data-aos-delay={idx * 300}
+                        className='item'>
+                        
+                        <h2>{t(`infos.${idx}.title`)}</h2>
+                        <p>{t(`infos.${idx}.description`)}</p>
+                        <ul>
+                            <li>{t(`infos.${idx}.list-description.list-item-one`)}</li>
+                            <li>{t(`infos.${idx}.list-description.list-item-two`)}</li>
+                            <li>{t(`infos.${idx}.list-description.list-item-three`)}</li>
+                            <li>{t(`infos.${idx}.list-description.list-item-four`)}</li>
+                        </ul>
+                        
+                    </div>))}
                 </Col>
+                <Col lg={16} md={5} sm={4} xs={4} className='infos-two'>
+                    {[3].map((idx) => (<div 
+                        key={idx}
+                        className='item'>
+                        
+                        <h2
+                            data-aos='fade-right'
+                            data-aos-easing='linear'
+                            data-aos-duration='1000'
+                        >
+                            {t(`infos.${idx}.title`)}
+                        </h2>
+
+                        {[0, 1].map((idnx) => (<div 
+                            key={idnx}
+                            data-aos='fade-right'
+                            data-aos-easing='linear'
+                            data-aos-duration='1000'
+                            data-aos-delay={idnx * 300}
+                        >
+                            <h3>{t(`infos.${idx}.sub-items.${idnx}.title`)}</h3>
+                            <p>{t(`infos.${idx}.sub-items.${idnx}.description`)}</p>
+                        </div>))}
+                    </div>))}
+                </Col>
+                <Col lg={16} md={5} sm={4} xs={4} className='infos-two'>
+                    {[4].map((idx) => (<div 
+                        key={idx}
+                        data-aos='fade-right'
+                        data-aos-easing='linear'
+                        data-aos-duration='1000'
+                        data-aos-delay={idx * 100}
+                        className='item'>
+                        
+                        <h2>{t(`infos.${idx}.title`)}</h2>
+                        <p>{t(`infos.${idx}.description`)}</p>
+                        
+                    </div>))}
+                </Col>
+                <Col lg={8} md={3} sm={4} xs={4} />
                 <Col lg={16} md={8} sm={4} xs={4} className='chart-row'>
                     <div className='chart-info'>
                         <p>{t('chart-info')}</p>
@@ -90,25 +143,16 @@ const RetailerPartnership = () => {
                        
                     </div>))}
                 </Col>
-                <Column lg={16} md={8} sm={4} xs={4} className='buttons'>
-                    <ButtonCustom action={() => {}} text={t('button')} />
-                    <ButtonCustom
-                        action={() => {}}
-                        iconDescription='E-Mail'
-                        hasIconOnly
-                        renderIcon={() => <img src={EmailIcon} alt='E-Mail' />}
-                    />
-                </Column>
                 <Column lg={16} md={8} sm={4} xs={4} className='logos'  
                     data-aos='fade-in' data-aos-easing='linear' data-aos-duration='1000' data-aos-delay='300'>
                     <div className='marquee'>
                         <div className='marquee__group'>
                             <img src={LouisVuittonLogo} alt='Louis Vuitton Logo' />
                             <img src={YSLLogo} alt='YSL Logo' />
-                            <img src={ChanelPradaLogo} alt='Chanel Prada Logo' />
+                            <img className='chanel-prada' src={CHANEL} alt='ChaneL Logo' />
+                            <img className='chanel-prada' src={PRADA} alt='Prada Logo' />
                             <img src={LouisVuittonLogo} alt='Louis Vuitton Logo' />
                             <img src={YSLLogo} alt='YSL Logo' />
-                            <img src={ChanelPradaLogo} alt='Chanel Prada Logo' />
                         </div>
                     </div>
                 </Column>
