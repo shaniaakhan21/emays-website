4ff05dce47e5731573ae3e1b0d3516f8a1342de0
thathsara <thathsararaviraj>
2023-05-11 18:26:46 +0400

    add all the components skeleton of the nav bar

---
+++ public/component/dashboard/deliveryOrder/DeliveryOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeliveryOrder = () => {
+    return (
+        <>
+            Del Orders...
+        </>
+    );
+};
+
+export default React.memo(DeliveryOrder);
