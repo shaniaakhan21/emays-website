1a68bacfcef787ad807d9c77a0c29606af11cc05
unknown <shaniaakhan21@gmail.com>
2023-06-20 12:29:23 +0400

    Cookie Banner and Cookie Policy

--- public/index.jsx
+++ public/index.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import './js/util/i18n';
 import App from './component/MainRouter';
+import CookieBanner from './component/CookieBanner';
 
 ReactDOM.createRoot(document.getElementById('app')).render(
     <React.StrictMode>
+        <CookieBanner/>
         <App />
     </React.StrictMode>
 );
