bc0f358e27e9ebb0d546196ea1bd64aea17ede59
thathsara <thathsararaviraj>
2023-10-06 17:35:20 +0400

    add history page functional changes backend and frontend

--- public/component/dashboard/redux/thunk/overviewThunk.js
+++ public/component/dashboard/redux/thunk/overviewThunk.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { loadOrders } from '../../../../services/dashboard/overview';
+import { loadOrderById, loadOrders } from '../../../../services/dashboard/overview';
 
 export const getOverviewData = createAsyncThunk('overview/getOverviewData', async (data, { getState }) => {
 
@@ -14,3 +14,14 @@ export const getOverviewData = createAsyncThunk('overview/getOverviewData', asyn
     }
 });
 
+export const getOverviewDataById = createAsyncThunk('overview/getOverviewDataById', async (data, { getState }) => {
+
+    const storeId = getState().appInfoState?.systemInfoState?.data?.id;
+    const authToken = getState().loginState.token;
+    const orderId = data?.orderId;
+    const response = await loadOrderById({ orderId: orderId
+        , storeId: storeId, token: authToken });
+    if (response) {
+        return response;
+    }
+});
--- public/services/dashboard/overview.js
+++ public/services/dashboard/overview.js
@@ -12,11 +12,31 @@ import { apiBase, HTTPHelper as httpUtil } from '../../js/util/httpUtil';
  */
 export const loadOrders = ({ pageNumber, pageLimit, storeId, token }) => {
     if (storeId) {
-        return httpUtil.get(`${apiBase}/orders?page=${pageNumber}&pageLimit=${pageLimit}&storeId=${storeId}`, {
+        // eslint-disable-next-line max-len
+        return httpUtil.get(`${apiBase}/orders/pagination?page=${pageNumber}&pageLimit=${pageLimit}&storeId=${storeId}`, {
             'Authorization': `Bearer ${token}`
         }, {});
     }
-    return httpUtil.get(`${apiBase}/orders?page=${pageNumber}&pageLimit=${pageLimit}`, {
+    return httpUtil.get(`${apiBase}/orders/pagination?page=${pageNumber}&pageLimit=${pageLimit}`, {
+        'Authorization': `Bearer ${token}`
+    }, {});
+};
+
+/**
+ * Load user orders by order id
+ * @param orderId {number}
+ * @param storeId {string}
+ * @param token {string}
+ * @returns {Promise<undefined|*>}
+ */
+export const loadOrderById = ({ orderId, storeId, token }) => {
+    if (storeId) {
+        // Point: branchId = storeId in our system
+        return httpUtil.get(`${apiBase}/orders/byOrderId/${orderId}?branchId=${storeId}`, {
+            'Authorization': `Bearer ${token}`
+        }, {});
+    }
+    return httpUtil.get(`${apiBase}/orders/byOrderId/${orderId}`, {
         'Authorization': `Bearer ${token}`
     }, {});
 };
