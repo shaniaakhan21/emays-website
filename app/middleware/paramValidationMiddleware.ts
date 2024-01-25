8858ae6ebb239b219188e2572bf765578e285a57
thathsara <thathsararaviraj>
2024-01-25 11:44:03 +0400

    remove product size from mandatory list

--- app/middleware/paramValidationMiddleware.ts
+++ app/middleware/paramValidationMiddleware.ts
@@ -121,8 +121,10 @@ export const validateCreateOrder = (req: Request, res: Response, next: NextFunct
                 return validatorErrorBuilder(err as Error, ADDRESS_REQUIRED); }),
             orderItems: Joi.array().items({
                 productName: Joi.string().required(),
-                // ProductColor: Joi.string().required(),
-                productSize: Joi.string().required(),
+                /*
+                 * ProductColor: Joi.string().required(),
+                 * productSize: Joi.string().required(),
+                 */
                 productQuantity: Joi.number().required(),
                 // Important: check currency value contains comma
                 productCost: Joi.string().disallow(',').required(),
