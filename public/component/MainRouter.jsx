8ad36a4644600166c895635b483447b6d48450dd
Gerardo Bastidas <gerardo_bastidas@hotmail.com>
2024-01-31 09:14:39 +0100

    fix: router

--- public/component/MainRouter.jsx
+++ public/component/MainRouter.jsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // SCSS
 import '../scss/main.scss';
